d32b2459b8d51810666de8286c31f37130642896
daladim <daladim@users.noreply.github.com>
2022-07-27 11:51:27 +0200

    [doc] SchemaKey and SchemaLocator

--- src/schema.rs
+++ src/schema.rs
@@ -27,14 +27,30 @@ impl From<tdh::TdhNativeError> for SchemaError {
 
 type SchemaResult<T> = Result<T, SchemaError>;
 
+/// A way to group events that share the same [`Schema`]
+///
+/// From the [docs](https://docs.microsoft.com/en-us/windows/win32/api/evntprov/ns-evntprov-event_descriptor):
+/// > For manifest-based ETW, the combination Provider.DecodeGuid + Event.Id + Event.Version should uniquely identify an event,
+/// > i.e. all events with the same DecodeGuid, Id, and Version should have the same set of fields with no changes in field names, field types, or field ordering.
 #[derive(Debug, Eq, PartialEq, Hash)]
 struct SchemaKey {
     // For now, lazy to wrap Guid around an implement Hash
     // TODO: wrap Guid and implement hash
     provider: String,
+    /// From the [docs](https://docs.microsoft.com/en-us/windows/win32/api/evntprov/ns-evntprov-event_descriptor): A 16-bit number used to identify manifest-based events
     id: u16,
-    opcode: u8,
+    /// From the [docs](https://docs.microsoft.com/en-us/windows/win32/api/evntprov/ns-evntprov-event_descriptor): An 8-bit number used to specify the version of a manifest-based event.
+    // The version indicates a revision to the definition of an event with a particular Id.
+    // All events with a given Id should have similar semantics, but a change in version
+    // can be used to indicate a minor modification of the event details, e.g. a change to
+    // the type of a field or the addition of a new field.
     version: u8,
+
+    // TODO: not sure why these ones are required in a SchemaKey. If they are, document why.
+    //       note that krabsetw also uses these fields (without an explanation)
+    //       however, krabsetw's `schema::operator==` do not use them to compare schemas for equality.
+    //       see https://github.com/microsoft/krabsetw/issues/195
+    opcode: u8,
     level: u8,
 }
 
@@ -61,7 +77,8 @@ impl SchemaKey {
 /// * EventHeader.EventDescriptor.Version
 /// * EventHeader.EventDescriptor.Level
 ///
-/// Credits: [KrabsETW::schema_locator](https://github.com/microsoft/krabsetw/blob/master/krabs/krabs/schema_locator.hpp)
+/// Credits: [KrabsETW::schema_locator](https://github.com/microsoft/krabsetw/blob/master/krabs/krabs/schema_locator.hpp).
+/// See also the [`SchemaKey`] for more info
 #[derive(Default)]
 pub struct SchemaLocator {
     schemas: HashMap<SchemaKey, Arc<TraceEventInfoRaw>>,
@@ -86,9 +103,9 @@ impl SchemaLocator {
     /// * `event` - The [EventRecord] that's passed to the callback
     ///
     /// # Remark
-    /// This is the first function that should be called within a Provider callback, if everything
-    /// works as expected this function will return a Result with the [Schema] that represents
-    /// the ETW event that triggered the callback
+    /// This is the first function that should be called within a Provider callback.
+    /// It consumes the ETW event and wrap it into a [Schema] that represents it
+    /// and provides a way to access its fields.
     ///
     /// This function can fail, if it does it will return a [SchemaError]
     ///
